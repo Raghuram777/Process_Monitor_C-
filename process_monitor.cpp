b0db68b8f77653d574e52b19dd6f58ceb52267ff
Raghuram777 <raghukanna88@gmail.com>
2025-06-23 14:51:12 +0530

    first commit

---
+++ process_monitor.cpp
@@ -0,0 +1,37 @@
+#include <windows.h>
+#include <tlhelp32.h>
+#include <iostream>
+
+int main() {
+    HANDLE hProcessSnap;
+    PROCESSENTRY32 pe32;
+    int i=0;
+
+    // Take a snapshot of all processes in the system
+    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    if (hProcessSnap == INVALID_HANDLE_VALUE) {
+        std::cerr << "Failed to take snapshot." << std::endl;
+        return 1;
+    }
+
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    // Get the first process
+    if (!Process32First(hProcessSnap, &pe32)) {
+        std::cerr << "Failed to get first process." << std::endl;
+        CloseHandle(hProcessSnap);
+        return 1;
+    }
+
+    std::cout << "PID\t\tProcess Name\n";
+    std::cout << "------------------------------\n";
+
+    do {
+        std::wcout << pe32.th32ProcessID << "\t\t" << pe32.szExeFile << std::endl;
+        i++;
+    } while (Process32Next(hProcessSnap, &pe32) && i<10);
+
+    CloseHandle(hProcessSnap);
+    return 0;
+}
